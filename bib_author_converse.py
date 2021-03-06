ec8939fba81c8c6b9f269e37f469d8f7e536e8f1
hirose-masahiro <hirose@ailab.ics.keio.ac.jp>
2021-03-06 11:22:35 +0900

    add bib_author_converse.py

---
+++ bib_author_converse.py
@@ -0,0 +1,35 @@
+print("please input authors:")
+
+str_input = input()
+str_input = str_input + ","
+
+mylist_1 = str_input.split(' ')  # 単語区切り
+mylist_2 = []  # 単語のindexのリストを入れるリスト
+mylist_tmp = []  # 単語のindexを入れるリスト
+
+# 人名を区切る
+for i in range(len(mylist_1)):
+    mylist_tmp.append(i)
+    if mylist_1[i] == "and":
+        mylist_tmp.pop()
+    elif mylist_1[i][-1:] == ",":
+        mylist_2.append(mylist_tmp.copy())
+        mylist_tmp.clear()
+
+str_output = ""
+# 人名を加工
+for i in range(len(mylist_2)):
+    if len(mylist_2[i]) == 2:
+        str_output += mylist_1[mylist_2[i][1]] + \
+            " " + mylist_1[mylist_2[i][0]]
+        if i != len(mylist_2) - 1:
+            str_output += " and "
+    elif len(mylist_2[i]) == 3:
+        str_output += mylist_1[mylist_2[i][1]] + ", " + \
+            mylist_1[mylist_2[i][2]] + " " + mylist_1[mylist_2[i][0]]
+        if i != len(mylist_2) - 1:
+            str_output += " and "
+
+print()
+print("authors in .bib style:")
+print(str_output)
