d8cd38edfd0cc18b0f4854e87803adb41c328e40
hirose-masahiro <hirose@ailab.ics.keio.ac.jp>
2021-03-06 13:10:51 +0900

    fix bug

--- bib_author_converse.py
+++ bib_author_converse.py
@@ -24,13 +24,14 @@ str_output = ""
 # 人名を加工
 for i in range(len(mylist_2)):
     if len(mylist_2[i]) == 2:
-        str_output += mylist_1[mylist_2[i][1]] + \
-            " " + mylist_1[mylist_2[i][0]]
+        str_output += mylist_1[mylist_2[i][1]] + " " + mylist_1[mylist_2[i][0]]
         if i != len(mylist_2) - 1:
             str_output += " and "
-    elif len(mylist_2[i]) == 3:
-        str_output += mylist_1[mylist_2[i][1]] + ", " + \
-            mylist_1[mylist_2[i][2]] + " " + mylist_1[mylist_2[i][0]]
+    elif len(mylist_2[i]) > 2:
+        for j in range(len(mylist_2[i]) - 2):
+            str_output += mylist_1[mylist_2[i][j] + 1] + ", "
+        str_output += mylist_1[mylist_2[i][-1]] + \
+            " " + mylist_1[mylist_2[i][0]]
         if i != len(mylist_2) - 1:
             str_output += " and "
 
